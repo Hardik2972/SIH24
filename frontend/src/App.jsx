0fdbb30c89ba8904fb26cc1fe621b7f63fad1a5b
Hardik2972 <garghardik2972@gmail.com>
2024-09-05 13:43:38 +0530

    done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,13 +4,14 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MuseumBookingPage from './components/MuseumBookingPage';
 import MainContainer from './components/chat/MainContainer';
 import Home from './components/Home/home';
-
+import ImageUploadComponent from './components/Image';
 function Root() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/booking" element={<MuseumBookingPage />} />
       <Route path="/gpt" element={<MainContainer />} />
+      <Route path="/image" element={<ImageUploadComponent />} />
     </Routes>
   );
 }
