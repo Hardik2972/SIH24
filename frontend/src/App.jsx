7284f8ddf4afc5ed8b8df9ec9eb6e555a68627bb
Ayush Gupta <ag8350961@gmail.com>
2024-09-05 13:59:25 +0530

    changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
+import VQA from './components/ImageUpload';
 import MuseumBookingPage from './components/MuseumBookingPage';
 import MainContainer from './components/chat/MainContainer';
 import Home from './components/Home/home';
@@ -10,7 +10,8 @@ function Root() {
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/booking" element={<MuseumBookingPage />} />
-      <Route path="/gpt" element={<MainContainer />} />
+      <Route path="/bot" element={<MainContainer />} />
+      <Route path="/vqa" element={<VQA />} />
     </Routes>
   );
 }
