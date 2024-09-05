81e63e50f8893bad4e0d592a63b5a4e414bbf232
Ayush Gupta <ag8350961@gmail.com>
2024-09-05 22:23:30 +0530

    changes

--- frontend/src/assets/helper.jsx
+++ frontend/src/assets/helper.jsx
@@ -2,33 +2,29 @@ import axios from 'axios';
 
 export const TranslateToAny = async (text, from, to) => {
     try {
+      // Check if translation is needed
+      console.log(from, text);
       if (from !== 'en') {
-        const translateOptions = {
-          method: 'POST',
-          url: import.meta.env.VITE_TRANSLATE_API_URL,
-          headers: {
-            'x-rapidapi-key': import.meta.env.VITE_TRANSLATE_API_KEY,
-            'x-rapidapi-host': 'google-translator9.p.rapidapi.com',
-            'Content-Type': 'application/json'
-          },
-          data: {
-            q: text,
-            source: from,
-            target: to,
-            format: 'text'
-          }
-        };
+        const formData = new FormData();
+        formData.append('key', import.meta.env.VITE_TRANSLATE_API_KEY); 
+        formData.append('sentence', text);
+        formData.append('src_lang', from);
+        formData.append('dest_lang', to);
   
-        const translateResponse = await axios.request(translateOptions);
-        return translateResponse.data.data.translations[0].translatedText;
+        const translateResponse = await axios.post(import.meta.env.VITE_TRANSLATE_API_URL, formData, {
+          headers: {
+            'Content-Type': 'multipart/form-data'
+          }
+        });
+
+        console.log(translateResponse.data.translated_text)
+
+        return translateResponse.data.translated_text; 
       } else {
-        return text;
+        return text; // No translation needed
       }
     } catch (error) {
       console.error('Error:', error);
       return null;
     }
 };
-
-
-
